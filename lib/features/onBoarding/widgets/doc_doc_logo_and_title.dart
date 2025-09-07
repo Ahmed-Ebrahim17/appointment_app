f2fa029f489ecfb545aaf41ef61bc7d760bef564
Ahmed-Ebrahim17 <ae447611@gmail.com>
2025-09-07 21:14:42 +0300

    splash screen created
    - added splash screen for IOS & Android below 12
    - added splash screen for Android 12+

---
+++ lib/features/onBoarding/widgets/doc_doc_logo_and_title.dart
@@ -0,0 +1,20 @@
+import 'package:appointment_app/core/theming/styels.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
+
+class DocDocLogoAndTitle extends StatelessWidget {
+  const DocDocLogoAndTitle({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        SvgPicture.asset("assets/svgs/dodoc_logo.svg"),
+        SizedBox(width: 10.w),
+        Text("Docdoc", style: TextStyles.font24Black700Weight),
+      ],
+    );
+  }
+}
