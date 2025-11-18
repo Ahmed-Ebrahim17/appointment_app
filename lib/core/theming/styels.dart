05677f2080152f3aefd5adfa1431339ef00de5d8
Ahmed-Ebrahim17 <ae447611@gmail.com>
2025-11-18 08:13:34 +0200

    - added topbar design. - aded color & textStyle. - added notifications svg.

--- lib/core/theming/styels.dart
+++ lib/core/theming/styels.dart
@@ -50,6 +50,11 @@ class TextStyles {
     fontWeight: FontWeightHelper.regular,
     color: ColorsManager.grey,
   );
+  static TextStyle font12RegularGrey = TextStyle(
+    fontSize: 12.sp,
+    fontWeight: FontWeightHelper.regular,
+    color: ColorsManager.darkGrey,
+  );
   static TextStyle font14LightGreyMedium = TextStyle(
     fontSize: 14.sp,
     fontWeight: FontWeightHelper.medium,
@@ -69,5 +74,11 @@ class TextStyles {
     fontSize: 16.sp,
     fontWeight: FontWeightHelper.semiBold,
     color: Colors.white,
+  );
+
+  static TextStyle font18BlackBold = TextStyle(
+    fontSize: 18.sp,
+    fontWeight: FontWeightHelper.bold,
+    color: ColorsManager.drakBlue,
   );
 }
---
+++ lib/features/home/ui/home_screen_ui.dart
@@ -0,0 +1,23 @@
+import 'package:appointment_app/features/home/ui/widgets/home_top_bar.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Container(
+          margin: EdgeInsets.fromLTRB(20, 16, 20, 28),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+            const HomeTopBar(),
+          ]),
+        ),
+      ),
+    );
+  }
+}
